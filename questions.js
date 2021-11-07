c7c3ca76af40861f23570b556aefbcacd4a828fb
Najla Parker <najla08@gmail.com>
2021-11-07 14:49:09 -0700

    Pushing placeholder questions .js file

---
+++ questions.js
@@ -0,0 +1,17 @@
+//Placeholder questions. TO DO: find actual questions for quiz
+
+const myQuestions = [
+    {
+        question: "djfkldjglkfdjg?",
+        possibleAnswer:
+        ["a","b","c"],
+        correctAnswer: 1
+    },
+    
+    {
+        question: "jglkfdjg?",
+        possibleAnswer:
+        ["a","b","c"],
+        correctAnswer: 0
+    }
+]
