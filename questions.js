573e2babbc2f67f245961f505977e77aad8d7b5c
Najla Parker <najla08@gmail.com>
2021-11-11 10:54:16 -0700

    Pushing new questions for quiz

--- questions.js
+++ questions.js
@@ -2,16 +2,21 @@
 
 const myQuestions = [
     {
-        question: "djfkldjglkfdjg?",
-        possibleAnswer:
-        ["a","b","c"],
-        correctAnswer: 1
+        question: "DOM stands for:",
+        choices: ["Document Object Module", "Document Object Model","Derivative Object Modification", "None of the Above"],
+        answer: 1,
     },
     
     {
-        question: "jglkfdjg?",
-        possibleAnswer:
-        ["a","b","c"],
-        correctAnswer: 0
+        question: "Which of the following is not a primitive data type in JavaScript?",
+        choices:
+        ["Boolean","String","Number", "All of the above are primitive data types in JavaScript"],
+        answer: 3,
+    },
+
+    {
+        question: "What will the following code return: Boolean(10>9):",
+        choices: ["NaN","True", "False", "None of the above"],
+        answer: 1,
     }
 ]
